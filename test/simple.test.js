024ee0e530598a816151bf8663fc27a948a1339e
indexzero <charlie.robbins@gmail.com>
2015-07-20 12:57:43 -0700

    [test] Added test coverage for basic entry handling.

---
+++ test/simple.test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var assert = require('assert'),
+    path = require('path'),
+    fs = require('fs'),
+    zlib = require('zlib'),
+    tar = require('tar'),
+    PackageBuffer = require('../npm-package-buffer');
+
+var fixturesDir = path.join(__dirname, 'fixtures');
+
+describe('npm-package-buffer simple', function () {
+  it('should untar and buffer a valid tar.Parse() stream', function (done) {
+    var tarFile = path.join(fixturesDir, 'npm-package-buffer-0.0.0.tgz');
+    var parser = tar.Parse();
+    var buffer = new PackageBuffer(parser);
+    var errState;
+
+    //
+    // Handle errors correctly by storing
+    // the error state in this scope
+    //
+    function onError(err) {
+      errState = err;
+    }
+
+    buffer
+      .on('error', onError)
+      .on('end', function () {
+        assert.ok(!errState);
+        assert.equal(typeof buffer.package, 'object');
+        assert.deepEqual(Object.keys(buffer.files), [
+          'package/.npmignore',
+          'package/README.md',
+          'package/LICENSE',
+          'package/npm-package-buffer.js',
+          'package/test/simple.test.js',
+          'package/test/fixtures/not-a-tarball.tgz',
+          'package/test/fixtures/npm-package-buffer-0.0.0.tgz'
+        ]);
+
+        done();
+      });
+
+    fs.createReadStream(tarFile)
+      .pipe(zlib.Unzip())
+      .on('error', onError)
+      .pipe(parser);
+  });
+});
